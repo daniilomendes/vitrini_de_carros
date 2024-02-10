b7445637ab7e50dc23e138a762bf4c823eb66180
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 15:24:05 -0300

    feat: add export em CustomFilter, SearchManufacturer, CarCard components

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,19 @@
-import Hero from './Hero'
-import CustomButton from './CustomButton'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
 
 export {
-    Hero,
-    CustomButton,
-    Navbar,
-    Footer
-}
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard
+};
