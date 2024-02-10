bb6195be868800ffdac4c5fc5f5babf08a98ca4d
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 16:47:51 -0300

    chore: removendo função dos tipos

--- types/index.ts
+++ types/index.ts
@@ -29,18 +29,3 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
-
-export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Preço base do aluguel por dia em dólares
-  const mileageFactor = 0.1; // Taxa adicional por quilômetro rodado
-  const ageFactor = 0.05; // Taxa adicional por ano de idade do veículo
-
-  // Calcular taxa adicional com base na quilometragem e idade
-  const mileageRate = city_mpg * mileageFactor;
-  const ageRate = (new Date().getFullYear() - year) * ageFactor;
-
-  // Calcular a taxa total de aluguel por dia
-  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-
-  return rentalRatePerDay.toFixed(0);
-};
