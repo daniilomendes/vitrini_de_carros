db209697495ad3a9894cccd6e5ade073b9f4eda2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 13:12:04 -0300

    feat: add lista de fabricantes, lista de ano em que foi produzido, lista de combustiveis e links do footer em constants

---
+++ constants/index.ts
@@ -0,0 +1,101 @@
+export const manufacturers = [
+    "Acura",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Buick",
+    "Cadillac",
+    "Chevrolet",
+    "Chrysler",
+    "Citroen",
+    "Dodge",
+    "Ferrari",
+    "Fiat",
+    "Ford",
+    "GMC",
+    "Honda",
+    "Hyundai",
+    "Infiniti",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "Lamborghini",
+    "Land Rover",
+    "Lexus",
+    "Lincoln",
+    "Maserati",
+    "Mazda",
+    "McLaren",
+    "Mercedes-Benz",
+    "MINI",
+    "Mitsubishi",
+    "Nissan",
+    "Porsche",
+    "Ram",
+    "Rolls-Royce",
+    "Subaru",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo",
+  ];
+  
+  export const yearsOfProduction = [
+    { title: "Ano", value: "" },
+    { title: "2015", value: "2015" },
+    { title: "2016", value: "2016" },
+    { title: "2017", value: "2017" },
+    { title: "2018", value: "2018" },
+    { title: "2019", value: "2019" },
+    { title: "2020", value: "2020" },
+    { title: "2021", value: "2021" },
+    { title: "2022", value: "2022" },
+    { title: "2023", value: "2023" },
+  ];
+  
+  export const fuels = [
+    {
+      title: "Gasolina",
+      value: "",
+    },
+    {
+      title: "Gás",
+      value: "Gás",
+    },
+    {
+      title: "Elétrico",
+      value: "Elétricidade",
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+      title: "Sobre",
+      links: [
+        { title: "Como funciona", url: "/" },
+        { title: "Apresentação", url: "/" },
+        { title: "Parceria", url: "/" },
+        { title: "Relações de trabalho", url: "/" },
+      ],
+    },
+    {
+      title: "Empresa",
+      links: [
+        { title: "Eventos", url: "/" },
+        { title: "Blog", url: "/" },
+        { title: "Podcast", url: "/" },
+        { title: "Convide um amigo", url: "/" },
+      ],
+    },
+    {
+      title: "Redes Sociais",
+      links: [
+        { title: "Discord", url: "/" },
+        { title: "Instagram", url: "/" },
+        { title: "Twitter", url: "/" },
+        { title: "Facebook", url: "/" },
+      ],
+    },
+  ];
