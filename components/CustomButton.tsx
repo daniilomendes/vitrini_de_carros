355e14fe33078b2a5194aef5536a9e1927771e45
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 15:24:43 -0300

    feat: add tipos textStyles, rightIcon e isDisabled

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
 const CustomButton = ({
   title,
   btnType,
   containerStyles,
   handleClick,
+  textStyles,
+  rightIcon,
+  isDisabled,
 }: CustomButtonProps) => {
   return (
     <button
@@ -15,7 +19,17 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="rigth icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
