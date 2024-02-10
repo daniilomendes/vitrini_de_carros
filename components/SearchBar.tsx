88ec68a3b2e930d3dd3b7c22d0f74a6b8633755a
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 14:08:35 -0300

    feat: add barra de pesquisa com nome de fabricação

---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useState } from "react";
+import { SearchManufacturer } from ".";
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('')
+
+
+  const handleSearch = () => {};
+  return <form action="" className="searchbar" onSubmit={handleSearch}>
+    <div className="searchbar__item">
+        <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
+    </div>
+  </form>;
+};
+
+export default SearchBar;
