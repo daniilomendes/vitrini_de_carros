5e32c7fb2c8115885058f2f58966495c84c1dd5d
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 15:16:10 -0300

    feat: add calculateCarRent, função para calcular o quanto o carro rende na cidade

--- types/index.ts
+++ types/index.ts
@@ -12,8 +12,7 @@ export interface SearchManufacturerProps {
   setManufacturer: (manufacturer: string) => void;
 }
 
-
-export interface CarCardProps {
+export interface CarProps {
   city_mpg: number;
   class: string;
   combination_mpg: number;
@@ -27,3 +26,18 @@ export interface CarCardProps {
   transmission: string;
   year: number;
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Preço base do aluguel por dia em dólares
+  const mileageFactor = 0.1; // Taxa adicional por quilômetro rodado
+  const ageFactor = 0.05; // Taxa adicional por ano de idade do veículo
+
+  // Calcular taxa adicional com base na quilometragem e idade
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calcular a taxa total de aluguel por dia
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
