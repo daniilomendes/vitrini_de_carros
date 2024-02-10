4711f0f267e98ef27c5fe5399713aa24c545a1dd
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 13:10:42 -0300

    style: garantindo que a div está tomando todo seu espaço

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,36 +1,32 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-    const handleScroll = () => {
-
-    }
+  const handleScroll = () => {};
 
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-          Encontre, reserve ou alugue um carro!
-        </h1>
+        <h1 className="hero__title">Encontre, reserve ou alugue um carro!</h1>
 
         <p className="hero__subtitle">
-          De forma fácil e rápida. Simplifique sua experiência de aluguel de carro com nosso processo de
-          reserva fácil.
+          De forma fácil e rápida. Simplifique sua experiência de aluguel de
+          carro com nosso processo de reserva fácil.
         </p>
 
-        <CustomButton 
-            title="Procurar carros"
-            containerStyles="bg-primary-blue text-white rounded-full mt-10"
-            handleClick={handleScroll}
+        <CustomButton
+          title="Procurar carros"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
-            <div className="hero__image-overlay" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
