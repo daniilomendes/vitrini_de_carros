46d46ccc34f4f1b422e4c343777d6ea52e40be7f
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 11:04:13 -0300

    feat: add Hero components, topo da página

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client"
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    const handleScroll = () => {
 
-export default Hero
+    }
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Encontre, reserve ou alugue um carro - de forma rápida e fácil!
+        </h1>
+
+        <p className="hero__subtitle">
+          Simplifique sua experiência de aluguel de carro com nosso processo de
+          reserva fácil.
+        </p>
+
+        <CustomButton 
+            title="Procurar carros"
+            containerStyles="bg-primary-blue text-white rounded-full mt-10"
+            handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+            <Image src="/hero.png" alt="hero" fill className="object-contain" />
+            <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
