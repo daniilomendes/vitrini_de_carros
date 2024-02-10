9f4c98e63434948487efbc86bbc66afc74b3578d
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-10 16:48:20 -0300

    feat: add CarCard components

--- utils/index.ts
+++ utils/index.ts
@@ -5,11 +5,26 @@ export async function fetchCars() {
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
     { headers: headers }
   );
 
-  const result = await response.json()
+  const result = await response.json();
 
-  return result
+  return result;
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Preço base do aluguel por dia em dólares
+  const mileageFactor = 0.1; // Taxa adicional por quilômetro rodado
+  const ageFactor = 0.05; // Taxa adicional por ano de idade do veículo
+
+  // Calcular taxa adicional com base na quilometragem e idade
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calcular a taxa total de aluguel por dia
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
