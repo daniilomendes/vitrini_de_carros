eec4441d34318971a2b5adc976f4b20732c6ab59
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-13 09:50:57 -0300

    feat: add SearchParams para realizar buscas, CustomFilter para filtrar as buscas em combústivel e ano, ShowMore component para mostrar mais carros

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
+import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -18,8 +26,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="Gasolina" />
-            <CustomFilter title="Ano" />
+            <CustomFilter title="Gasolina" options={fuels} />
+            <CustomFilter title="Ano" options={yearsOfProduction} />
           </div>
         </div>
 
@@ -30,6 +38,11 @@ export default async function Home() {
                 <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
